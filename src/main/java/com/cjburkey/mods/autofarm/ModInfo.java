38506bf6076c5e5494d607d3e757bdee19fea43c
CJ Burkey <cjburkey01@gmail.com>
2017-05-29 18:41:59 -0400

    Move from release to alpha .-.

--- src/main/java/com/cjburkey/mods/autofarm/ModInfo.java
+++ src/main/java/com/cjburkey/mods/autofarm/ModInfo.java
@@ -4,7 +4,7 @@ public class ModInfo {
 	
 	public static final String NAME = "AutofarmCraft";
 	public static final String MODID = "autofarm";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "0.0.1";
 	public static final String PACKAGE_BASE = "com.cjburkey.mods.autofarm";
 	public static final String PROXY_PACKAGE = PACKAGE_BASE + ".proxy";
 	
